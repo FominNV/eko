4254cadd442d75d0636c4483265c1f9c4df73d21
Nikolai Fomin <kk309@mail.ru>
2022-09-09 20:19:14 +0300

    create Section3 component

---
+++ src/components/MainPageBlock/Section3/index.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+import "./styles.scss";
+
+const Section3: FC = () => (
+  <section className="Section3">
+    <p className="Section3__text">Трансляция организована в удобном формате вебинара. Для участия вам необходимо быть зарегистрированным пользователем на YouTube и пройти регистрацию, чтобы получить ссылку на вебинар.</p>
+  </section>
+);
+
+export default Section3;
