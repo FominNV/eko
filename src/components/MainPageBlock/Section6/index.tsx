8e65ec5b3a1eff2d0eedd0a8170a5216b2905e0e
Nikolai Fomin <kk309@mail.ru>
2022-09-09 20:20:13 +0300

    create Section6 component

---
+++ src/components/MainPageBlock/Section6/index.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+import "./styles.scss";
+
+const Section6: FC = () => (
+  <div className="Section6">
+    <p className="Section6__text">СКИДКИ ДЕЙСТВУЮТ С 16 АВГУСТА ПО 31 ДЕКАБРЯ 2020 Г.</p>
+  </div>
+);
+
+export default Section6;
